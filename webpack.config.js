f064c34cacd082f909c3b8db24004d700777fa01
beto <jooseplugo@gmail.com>
2017-06-02 21:51:35 -0500

    Add Structure

---
+++ webpack.config.js
@@ -0,0 +1,137 @@
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = nodeEnv === 'production';
+
+const sourcePath = path.join(__dirname, './src');
+
+//common plugins
+const plugins = [
+    new HtmlWebpackPlugin({
+        hash: true,
+        template: path.join(sourcePath, 'index.html'),
+        filename: 'index.html',
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+]
+
+//common rules
+const rules = [
+    {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2016', 'es2017', 'react'],
+                plugins: ['transform-es2015-modules-commonjs']
+            }
+        }
+    },
+    {
+        test: /\.(png|gif|jpe?g|svg|ico)$/,
+        use: 'url-loader?limit=20480&name=assets/[name]-[hash].[ext]',
+    },
+    {
+        test: /\.(svg|woff|woff2|ttf|eot)$/,
+        exclude: /node:modules/,
+        use: 'url-loader?limit=100000&name=fonts/[name].[ext]'
+    }
+]
+
+if(isProduction) {
+  // Production plugins
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+    new ExtractTextPlugin('style-[hash].css')
+  );
+  //Production Rules
+  rules.push(
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader!postcss-loader!sass-loader',
+      }),
+    }
+  );
+} else {
+    // Development plugins
+    plugins.push(
+        new webpack.HotModuleReplacementPlugin(),
+        new DashboardPlugin(),
+        new OpenBrowserPlugin({ url: 'http://localhost:9000' })
+    );
+
+    // Development rules
+    rules.push(
+        {
+            test: /\.scss$/,
+            exclude: /node_modules/,
+            use: ['style-loader', 'css-loader', 'sass-loader?sourceMap', 'postcss-loader']
+        }
+    )
+}
+
+module.exports = {
+    devtool: isProduction ? false : 'source-map',
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins,
+    devtool: "source-map",
+    module: {
+        rules
+    },
+    resolve: {
+        extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        historyApiFallback: true,
+        port: 9000,
+        compress: isProduction,
+        inline: !isProduction,
+        host: '0.0.0.0',
+        disableHostCheck: true,
+        stats: {
+            assets: true,
+            children: false,
+            chunks: false,
+            hash: false,
+            modules: false,
+            publicPath: false,
+            timings: true,
+            version: false,
+            warnings: true,
+            colors: {
+                green: '\u001b[32m',
+            },
+        },
+    }
+}
