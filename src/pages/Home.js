b44f14a849254a34de0fa37dfe38d4d77f7692ee
beto <jooseplugo@gmail.com>
2017-06-03 19:12:29 -0500

    Add home style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+import 'stylesPages/home.scss';
+import fondo from './../assets/images/pexels2.jpeg';
+import name from './../assets/images/name.png';
+
 class Home extends Component {
 
     render(){
         return(
-            <div>
-                Welcome to Home
+            <div className="home" style={{backgroundImage: 'url(' + fondo + ')'}}>
+                <div className="home-container">
+                    <img className="home-container-img" src={name} alt=""/>
+                    <p className="home-container-text">I'm a Front-End web developer
+                        based in Saltillo Coahuila, Mexico.
+                      I like to work with the latest tools and methodologies by keeping up
+                        to date with current trends.</p>
+                </div>
             </div>
         )
     }
