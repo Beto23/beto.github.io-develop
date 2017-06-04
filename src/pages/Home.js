125fd719329d7ef68107e6e95c0e3843e242f756
beto <jooseplugo@gmail.com>
2017-06-04 16:51:32 -0500

    Add home responsive se agrego flexboxgrid

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 import 'stylesPages/home.scss';
 import fondo from './../assets/images/pexels3.jpeg';
-import name from './../assets/images/name.png';
+import welcomeLarge from './../assets/images/welcome3.png';
+import welcomeDefault from '../assets/images/betocel.png';
 
 import FooterHome from '../components/FooterHome';
 
@@ -12,7 +13,12 @@ class Home extends Component {
         return(
             <div className="home" style={{backgroundImage: 'url(' + fondo + ')'}}>
                 <div className="home-container">
-                    <img className="home-container-img" src={name} alt=""/>
+                    <picture>
+                        <source srcSet={welcomeLarge} media="(min-width: 670px)" />
+                        <source srcSet={welcomeDefault}/>
+                        <img className="home-container-img" src={welcomeLarge} alt=""/>
+                    </picture>
+                    {/*<img className="home-container-img" src={nameCel} alt=""/>*/}
                     <p className="home-container-text">I'm a Front-End web developer
                         based in Saltillo Coahuila, Mexico.
                       I like to work with the latest tools and methodologies by keeping up
