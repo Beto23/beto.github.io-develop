f58eb344dbb960628dc325e4497e2ca2f0210a00
beto <jooseplugo@gmail.com>
2017-06-05 22:49:23 -0500

    Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,29 @@ import '../styles/components/Header.scss';
 
 class Header extends Component {
 
+    constructor(){
+        super();
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
     render() {
+        const { isOpen } = this.state;
         return(
             <header className="header">
-                Logo
+                <button className="header-button" onClick={this.handleClick}>
+                    <i className={(isOpen ? 'icon-close': 'icon-menu')}></i>
+                </button>
+                <div className={"header-container " + (isOpen ? 'header-container__show': '') }>
+                    {/* poner display:none al contenido */}
+                </div>
             </header>
         )
     }
