73cea9101c12fff3dff8d17b666b93ebcb3e04e5
Jose Alberto <josee-45@hotmail.com>
2018-05-02 23:49:34 -0500

    Add custom cursor

--- app/app.tsx
+++ app/app.tsx
@@ -3,12 +3,42 @@ import { render } from 'react-dom';
 
 import './styles/common.scss'
 
+interface IState {
+    pageX: number,
+    pageY: number
+}
+
 export default class App extends React.Component {
 
+    state: IState = {
+        pageX: 0,
+        pageY: 0
+    }
+
+    cursorEl: HTMLElement;
+
+    componentDidMount() {
+        document.onmousemove = this.handleCustomCursor;
+    }
+
+    handleCustomCursor = (e: MouseEvent) => {
+        this.setState({
+            pageX: e.pageX,
+            pageY: e.pageY
+        });
+    }
+
     render() {
+        const { pageX, pageY } = this.state;
         return (
             <div>
                 Hola React
+                <div
+                    className="b-cursor-custom"
+                    style={{
+                        left: pageX,
+                        top: pageY
+                    }}></div>
             </div>
         );
     }
